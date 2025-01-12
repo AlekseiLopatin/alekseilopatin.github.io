56228a41ad8167f646c20f09b0b98553db81704f
Aleksei Lopatin <badhead92@mail.ru>
2025-01-13 06:02:33 +0700

    added cahnge background color

--- scriptCalories.js
+++ scriptCalories.js
@@ -99,3 +99,32 @@ function clearForm() {
 addEntryButton.addEventListener("click", addEntry);
 calorieCounter.addEventListener("submit", calculateCalories);
 clearButton.addEventListener("click", clearForm);
+
+const darkColorsArr = [
+  "#2C3E50",
+  "#34495E",
+  "#2C2C2C",
+  "#616A6B",
+  "#4A235A",
+  "#2F4F4F",
+  "#0E4B5A",
+  "#36454F",
+  "#2C3E50",
+  "#800020",
+];
+
+function getRandomIndex() {
+  const randomIndex = Math.floor(darkColorsArr.length * Math.random());
+  return randomIndex;
+}
+
+const body = document.querySelector("body");
+
+function changeBackgroundColor() {
+  const color = darkColorsArr[getRandomIndex()];
+
+  body.style.backgroundColor = color;
+}
+const btn = document.querySelector("#background");
+
+btn.onclick = changeBackgroundColor;
