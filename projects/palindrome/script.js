905c40807a0be02ef4237f37d46f6d20e99004c3
Aleksei Lopatin <badhead92@mail.ru>
2025-02-27 21:21:58 +0700

    Uploaded palindrome

---
+++ projects/palindrome/script.js
@@ -0,0 +1,18 @@
+document.getElementById('check-btn').addEventListener('click', function() {
+    const inputText = document.getElementById('text-input').value.trim();
+    const resultDiv = document.getElementById('result');
+
+    if (inputText === '') {
+        alert('Please input a value');
+        return;
+    }
+
+    const cleanedInput = inputText.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedInput = cleanedInput.split('').reverse().join('');
+
+    if (cleanedInput === reversedInput) {
+        resultDiv.textContent = `${inputText} is a palindrome!`;
+    } else {
+        resultDiv.textContent = `${inputText} is not a palindrome.`;
+    }
+});
