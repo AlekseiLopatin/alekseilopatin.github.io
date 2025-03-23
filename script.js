7f4fa11c6d96ab5e7aec1301ec6251ca963bb57f
Aleksei Lopatin <badhead92@mail.ru>
2025-03-23 10:59:33 +0700

    Cleaned up the files and the folders

---
+++ script.js
@@ -0,0 +1,9 @@
+const def1 = document.getElementById('def1');
+const def2 = document.getElementById('def2');
+const def3 = document.getElementById('def3');
+const def4 = document.getElementById('def4');
+
+window.setTimeout(() => {def1.classList.toggle('hide')}, 500);
+window.setTimeout(() => {def2.classList.toggle('hide')}, 1000);
+window.setTimeout(() => {def3.classList.toggle('hide')}, 1500);
+window.setTimeout(() => {def4.classList.toggle('hide')}, 2000);
